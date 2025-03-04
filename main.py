587fa5abac26bec10685edcd56b21d378c21a92e
Luis Jaramillo <luis.jaramillo@payu.com>
2025-03-03 21:33:14 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+import os
+import random
+import sys
+
+pygame.init()
+
+# Global Constants
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+RUNNING = [pygame.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
+           pygame.image.load(os.path.join("Assets/Dino", "DinoRun2.png"))]
+
+JUMPING = pygame.image.load(os.path.join("Assets/Dino", "DinoJump.png"))
+
+SMALL_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus1.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus2.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus3.png"))]
+LARGE_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus1.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus2.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus3.png"))]
+
+BG = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
+
+FONT = pygame.font.Font('freesansbold.ttf', 20)
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        SCREEN.fill((255, 255, 255))
+        clock.tick(30)
+        pygame.display.update()
+
+
+main()
